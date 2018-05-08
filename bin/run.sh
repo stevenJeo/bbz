7d22fdbebc83defdf71cbb635d47177bd8a6711b
zhishuai.zhou <zhishuai.zhou@wormpex.com>
2018-05-08 19:02:58 +0800

    git文件

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+pwd
+
+host=`hostname`
+
+echo ${host}
+
+lsof -i:8888
+
+echo ""
+
+# awk中 NR行判断，print $2打印指定列
+port_info=lsof -i:8888 | awk '{if (NR>1){print NR " " $1 " " $2}}'
+
+echo ${port_info}
+
+
+
+#home=$(cd `dirname $0`; pwd)
+#echo "home=${home}"
+
+
+# awk 中 -F '/' 分隔符
+# awk 中 -F '/' 分隔符
+#project_name=`echo ${home} | awk -F'/' '{print $(NF-2)}'`
+#echo "project_name = ${project_name}"
+
+
+
